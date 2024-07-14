ec5fec6d476f768d9039db7c7b5311c8f89211b7
Hminchae <hminchae@gmail.com>
2024-07-14 23:50:40 +0900

    Add : 시드파일 생성
    - 코드베이스로 파일 준비
    - MVVM 구조로 폴더 생성

---
+++ Weather/Weather/Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by 황민채 on 7/14/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
