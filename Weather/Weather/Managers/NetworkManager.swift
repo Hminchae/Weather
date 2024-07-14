0e2adf3c80a1707e3324dd1216289af0bdaaa33f
Hminchae <hminchae@gmail.com>
2024-07-15 00:04:12 +0900

    Add : 네트워크 통신 매니저 구현

---
+++ Weather/Weather/Managers/NetworkManager.swift
@@ -0,0 +1,35 @@
+//
+//  NetworkManager.swift
+//  Weather
+//
+//  Created by 황민채 on 7/15/24.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() { }
+    
+    func getWeather(lat: Double, lon: Double, completionHandler: @escaping (OpenWeather) -> Void) {
+        let url = Constants.fullURL
+        let para: Parameters = [
+            "lat": lat,
+            "lon": lon,
+            "appid": Constants.API_KEY,
+            "units": "metric"
+        ]
+        
+        AF.request(url, parameters: para).responseDecodable(of: OpenWeather.self) { response in
+            switch response.result {
+            case .success(let value):
+                completionHandler(value)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
