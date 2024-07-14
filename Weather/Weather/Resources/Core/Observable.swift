62c9913475110572e093bd0c3b97b5de5e1ed014
Hminchae <hminchae@gmail.com>
2024-07-14 23:56:31 +0900

    Add : Custom Observable 구현

---
+++ Weather/Weather/Resources/Core/Observable.swift
@@ -0,0 +1,28 @@
+//
+//  Observable.swift
+//  Weather
+//
+//  Created by 황민채 on 7/14/24.
+//
+
+import Foundation
+
+class Observable<T> {
+    
+    private var closure: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            print("didSet")
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T) -> Void) {
+        self.closure = closure
+    }
+}
