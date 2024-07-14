ae9b33a05f28be12b0d8457025e75ce6602c3a6b
Hminchae <hminchae@gmail.com>
2024-07-14 23:53:09 +0900

    Add : Base vc, cell 생성
    - BaseViewController
    - BaseTableViewCell
    - BaseCollectionViewCell

---
+++ Weather/Weather/Resources/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  BaseCollectionViewCell.swift
+//  Weather
+//
+//  Created by 황민채 on 7/14/24.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .white
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() { }
+    func configureLayout() { }
+    func configureView() { }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Weather/Weather/Resources/Base/BaseViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BaseViewController.swift
+//  Weather
+//
+//  Created by 황민채 on 7/14/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+    
+    func configureView() {
+        
+    }
+}
