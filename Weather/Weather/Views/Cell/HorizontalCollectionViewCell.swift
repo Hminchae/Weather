f1876337bd5a4158b8dd84b756d01a5f492889d3
Hminchae <hminchae@gmail.com>
2024-07-15 03:00:26 +0900

    Add : HorizontalCollectionViewCell 레이아웃 구현

---
+++ Weather/Weather/Views/Cell/HorizontalCollectionViewCell.swift
@@ -0,0 +1,70 @@
+//
+//  HorizontalCollectionViewCell.swift
+//  Weather
+//
+//  Created by 황민채 on 7/15/24.
+//
+
+import UIKit
+
+import Kingfisher
+
+final class HorizontalCollectionViewCell: BaseCollectionViewCell {
+    
+    private let hourLabel = {
+        let label = UILabel()
+        label.font = WEFont.r13
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private let weatherIcon = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    private let tempLabel = {
+        let label = UILabel()
+        label.font = WEFont.m14
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    override func configureHierarchy() {
+        addSubview(hourLabel)
+        addSubview(weatherIcon)
+        addSubview(tempLabel)
+    }
+    
+    override func configureLayout() {
+        hourLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(4)
+        }
+        
+        weatherIcon.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(hourLabel.snp.bottom).offset(4)
+            make.size.equalTo(25)
+        }
+
+        tempLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(weatherIcon.snp.bottom).offset(4)
+        }
+    }
+    
+    override func configureView() {
+        
+    }
+    
+    public func configure(hour: String, icon: String, temp: Double) {
+        hourLabel.text = hour
+        weatherIcon.kf.setImage(with: URL(string: "\(Constants.iconURL)\(icon)\(Constants.iconSize)"))
+        tempLabel.text = String(Int(temp))
+    }
+}
